e3783999b42cb024204ef7ea8752c1a5a81062a6
Sue Le Marinel <sue.lemarinel@gmail.com>
2025-10-31 11:50:02 +0100

    updates

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ var yearRouter = require('./routes/year');
 var app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+reviver: (key, value) => {
+if (key === 'year' && typeof value === 'number') {
+return String(value);
+}
+return value;
+}
+}));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', yearRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
