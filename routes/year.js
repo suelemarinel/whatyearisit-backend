9147041a0bf7ff768808488047cb4bbc36672cd2
Sue Le Marinel <sue.lemarinel@gmail.com>
2025-10-31 11:30:58 +0100

    updates

--- routes/year.js
+++ routes/year.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 // GET /year
 router.get('/', function(req, res, next) {
-  const currentYear = new Date().getFullYear();
-  res.json({ year: "2025" });
+  const currentYear = new Date();
+  res.json({ year: currentYear.getFullYear().toString() });
 });
 
 module.exports = router;
