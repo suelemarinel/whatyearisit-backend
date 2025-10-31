76bcc3bbdf2fa2971524d90d48700e1be1543a7b
Sue Le Marinel <sue.lemarinel@gmail.com>
2025-10-31 11:26:03 +0100

    Test string

--- routes/year.js
+++ routes/year.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // GET /year
 router.get('/', function(req, res, next) {
   const currentYear = new Date().getFullYear();
-  res.json({ year: currentYear.toString() });
+  res.json({ year: "2025" });
 });
 
 module.exports = router;
